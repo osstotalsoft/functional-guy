531e22a1682ab893fca7880c1df8c60c55fa18cc
Vasile Cuzmin <vcuzmin@totalsoft.ro>
2021-01-07 10:09:39 +0200

    Do notation and monadic operators versions of an example have been added

--- Chapter4. IO in Haskell/Exercises/01. Basic IO Operations/Hello.hs
+++ Chapter4. IO in Haskell/Exercises/01. Basic IO Operations/Hello.hs
@@ -1,22 +1,31 @@
-import Data.Char(toUpper)
+import Data.Char (toUpper)
 
 helloPerson :: String -> String
 helloPerson name = "Hello" ++ " " ++ name ++ "!"
 
 main :: IO ()
 
--- main = do
---     putStrLn "Hello! What's your name?"
---     name <- getLine
---     let statement = helloPerson name
---     putStrLn statement
+main' :: IO ()
+main' =
+  putStrLn "Hello! What's your name?"
+    >> getLine
+    >>= ( \firstName ->
+            putStrLn "What's your last name?"
+              >> getLine
+              >>= ( \lastName ->
+                      let bigFirstName = map toUpper firstName
+                          bigLastName = map toUpper lastName
+                       in putStrLn $ "hey " ++ bigFirstName ++ " " ++ bigLastName ++ ", how are you?"
+                  )
+        )
 
 
 main = do
-    putStrLn "What's your first name?"
-    firstName <- getLine
-    putStrLn "What's your last name?"
-    lastName <- getLine
-    let bigFirstName = map toUpper firstName
-        bigLastName = map toUpper lastName
-    putStrLn $ "hey " ++ bigFirstName ++ " " ++ bigLastName ++ ", how are you?"
+  putStrLn "What's your first name?"
+  firstName <- getLine
+
+  putStrLn "What's your last name?"
+  lastName <- getLine
+  let bigFirstName = map toUpper firstName
+      bigLastName = map toUpper lastName
+  putStrLn $ "hey " ++ bigFirstName ++ " " ++ bigLastName ++ ", how are you?"
