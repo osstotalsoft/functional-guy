c5d670209588e075ae695057811f4cfcfea42233
Radu Popovici <rpopovici@totalsoft.ro>
2021-07-20 10:43:29 +0300

    minor changes

--- Chapter7. FSharp microservices/Sample/NBB.Invoices.FSharp/NBB.Invoices.FSharp.Api/HttpHandlers.fs
+++ Chapter7. FSharp microservices/Sample/NBB.Invoices.FSharp/NBB.Invoices.FSharp.Api/HttpHandlers.fs
@@ -1,52 +1,35 @@
-namespace NBB.Invoices.FSharp.Api
+module NBB.Invoices.FSharp.Api.HttpHandlers
+
+open Microsoft.AspNetCore.Http
+open FSharp.Control.Tasks
+open Giraffe
+open NBB.Invoices.FSharp.Application
+open NBB.Core.Effects
+open System
+open NBB.Application.Mediator.FSharp
 
 
-// type HttpFuncResult = Task<HttpContext option>
-// type HttpFunc = HttpContext -> HttpFuncResult
-// type HttpHandler = HttpFunc -> HttpContext -> HttpFuncResult
+let asyncCommandResponse : HttpHandler =
+    let response =
+        {| CommandId = Guid.NewGuid()
+           CorrelationId = Guid.NewGuid() |}
 
-module HttpHandlers =
+    json response
 
-    open Microsoft.AspNetCore.Http
-    open FSharp.Control.Tasks
-    open Giraffe
-    open NBB.Invoices.FSharp.Api.Models
-    open NBB.Invoices.FSharp.Application
-    open NBB.Core.Effects
-    open System
-    open NBB.Application.Mediator.FSharp
+let sendCommand (cmd: 'a :> ICommand) (next: HttpFunc) (ctx: HttpContext) =
+    task {
+        let effect = Mediator.sendCommand cmd
+        let interpreter = ctx.GetService<IInterpreter>()
+        do! interpreter.Interpret(effect)
 
+        return! next ctx
+    }
 
-
-
-    let handleGetHello =
-        fun (next: HttpFunc) (ctx: HttpContext) ->
-            task {
-                let response = { Text = "Hello world, from Giraffe!" }
-                return! json response next ctx
-            }
-
-    let asyncCommandResponse : HttpHandler =
-        let response =
-            {| CommandId = Guid.NewGuid()
-               CorrelationId = Guid.NewGuid() |}
-
-        json response
-
-    let sendCommand (cmd: 'a :> ICommand) (next: HttpFunc) (ctx: HttpContext) =
-        task {
-            let effect = Mediator.sendCommand cmd
-            let interpreter = ctx.GetService<IInterpreter>()
-            do! interpreter.Interpret(effect)
-
-            return! next ctx
-        }
-
-    let cmd<'a when 'a :> ICommand> =
-        bindJson<'a> sendCommand >=> asyncCommandResponse
+let cmd<'a when 'a :> ICommand> =
+    bindJson<'a> sendCommand >=> asyncCommandResponse
 
 
-    let invoiceHandler : HttpHandler =
-        POST
-        >=> choose [ route "/create" >=> cmd<CreateInvoice.Command>
-                     route "/pay" >=> cmd<MarkInvoiceAsPayed.Command> ]
+let invoiceHandler : HttpHandler =
+    POST
+    >=> choose [ route "/create" >=> cmd<CreateInvoice.Command>
+                 route "/pay" >=> cmd<MarkInvoiceAsPayed.Command> ]
--- Chapter7. FSharp microservices/Sample/NBB.Invoices.FSharp/NBB.Invoices.FSharp.Api/Models.fs
+++
@@ -1,7 +0,0 @@
-namespace NBB.Invoices.FSharp.Api.Models
-
-[<CLIMutable>]
-type Message =
-    {
-        Text : string
-    }
