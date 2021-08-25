117492379cae959c49d3b5321a2bea7df3a775f6
Radu Popovici <rpopovici@totalsoft.ro>
2021-08-25 15:40:38 +0300

    a lot of combinators

--- Chapter8. JavaScript libraries/Sample/pure-validations/src/__tests__/combinators.test.js
+++ Chapter8. JavaScript libraries/Sample/pure-validations/src/__tests__/combinators.test.js
@@ -1,13 +1,73 @@
-import {required, email} from '../primitiveValidators'
-import {stopOnFirstFailure} from '../combinators'
-import {ValidationResult} from '../algebra'
+import { required, email } from '../primitiveValidators'
+import { stopOnFirstFailure, concatFailure, error, field, shape, items, allProps } from '../combinators'
+import { ValidationResult } from '../algebra'
+import { $do } from '@totalsoft/zion'
+import { map, concat } from 'ramda'
 
-const {/*Success,*/ Failure} = ValidationResult
+const { Success, Failure } = ValidationResult
 
 describe("combinators tests:", () => {
     it("stopOnFirstFailure: ", () => {
         const validator = stopOnFirstFailure([required, email])
         expect(validator(null)).toStrictEqual(Failure(["required"]))
         expect(validator("notAnEmail")).toStrictEqual(Failure(["not an email"]))
+    })
+
+    it("concatFailure: ", () => {
+        const validator = concatFailure([required, email])
+        expect(validator(null)).toStrictEqual(Failure(["required", "not an email"]))
+        expect(validator("notAnEmail")).toStrictEqual(Failure(["not an email"]))
+    })
+
+    it("do notation: ", () => {
+        const validator = x => $do(function* () {
+            const x1 = yield required(x)
+            return yield email(x1 + "@totalsoft.ro")
+        })
+        expect(validator(null)).toStrictEqual(Failure(["required"]))
+        expect(validator("notAnEmail")).toStrictEqual(Success("notAnEmail@totalsoft.ro"))
+    })
+
+    it("error: ", () => {
+        const validator = required |> error(map(concat("*")));
+        expect(validator(null)).toStrictEqual(Failure(["*required"]))
+        expect(validator("something")).toStrictEqual(Success("something"))
+    })
+
+    it("field: ", () => {
+        const validator = required |> field("name")
+        expect(validator({ name: null })).toStrictEqual(Failure(["name: required"]))
+        expect(validator({ name: "radu" })).toStrictEqual(Success({ name: "radu" }))
+    })
+
+    it("shape: ", () => {
+        const validator = shape({ name: required, email: email })
+        expect(validator({ name: null, email: "notAnEmail" })).toStrictEqual(Failure(["name: required", "email: not an email"]))
+        //expect(validator({name:"radu"})).toStrictEqual(Success({name:"radu"}))
+    })
+
+    it("items: ", () => {
+        const validator = required |> items
+        expect(validator([null, null])).toStrictEqual(Failure(["0: required", "1: required"]))
+        expect(validator(["null", null])).toStrictEqual(Failure(["1: required"]))
+    })
+
+    it("allProps: ", () => {
+        const validator = required |> allProps
+        expect(validator({name: null, surname: null, age: null})).toStrictEqual(Failure(["name: required", "surname: required", "age: required"]))
+    })
+
+    it("composed validator: ", () => {
+        const validator = shape({
+            name: required |> error(map(concat("*"))),
+            email: [required, email] |> stopOnFirstFailure,
+            addresses: required |> items
+        })
+        expect(validator({
+            name: "",
+            email: "wrongEmail",
+            addresses: ["some addr", ""]
+        })).toStrictEqual(Failure(["name: *required", "email: not an email", "addresses: 1: required"]))
+        //expect(validator({name:"radu"})).toStrictEqual(Success({name:"radu"}))
     })
 })
--- Chapter8. JavaScript libraries/Sample/pure-validations/src/combinators.js
+++ Chapter8. JavaScript libraries/Sample/pure-validations/src/combinators.js
@@ -1,7 +1,59 @@
-import { composeK as _composeK, flip, reduce } from 'ramda'
+import { pipeK, reduce, map, applyTo, liftN, always, identity, apply, curry, concat, toPairs, range, length, keys } from 'ramda'
 import { ValidationResult } from './algebra'
+import { $do } from '@totalsoft/zion'
 
-const { Success } = ValidationResult
-const composeK = flip(_composeK)
+const { Success, Failure } = ValidationResult
+const composeK = pipeK
 
 export const stopOnFirstFailure = reduce(composeK, Success)
+
+const concatFailure2 = (v1, v2) => x =>
+    [v1, v2]
+    |> map(applyTo(x))
+    |> apply(liftN(2, always(identity)))
+
+// const concatFailure2 = (v1, v2) => x =>{
+//     const vr1 = v1(x)
+//     const vr2 = v2(x)
+//     const takeLast = curry((_,y) => y)
+//     const liftA2 = liftN(2)
+//     const takeLastVr = liftA2(takeLast)
+//     return takeLastVr(vr1, vr2)
+// }
+
+export const concatFailure = reduce(concatFailure2, Success)
+
+export const error = curry((errorsMapFn, v) => x =>
+    v(x).cata({
+        Success: Success,
+        Failure: errors => Failure(errors |> errorsMapFn)
+    })
+)
+
+export const field = curry((key, fieldValidator) => x => $do(function* () {
+    const fieldValue = x[key]
+    const v = fieldValidator |> error(map(concat(`${key}: `)))
+    yield v(fieldValue)
+    return x
+}))
+
+export const shape = shapeValidator =>
+    shapeValidator
+    |> toPairs // [[key, validator]]
+    |> map(apply(field)) // [ValidationResult a]
+    |> concatFailure //ValidationResult a
+
+export const items = itemValidator => x =>
+    x
+    |> length
+    |> range(0) // [0..length x]
+    |> map(i => field(i, itemValidator)) // [Validator a]
+    |> concatFailure
+    |> applyTo(x)
+
+export const allProps = propValidator => x =>
+    x
+    |> keys
+    |> map(k => field(k, propValidator)) // [Validator a]
+    |> concatFailure
+    |> applyTo(x)
